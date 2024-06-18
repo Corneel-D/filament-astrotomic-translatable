0e14ca08465f13c7a13e9480bf1ff1185bc96e2e
Corneel D <corneel.d.git@gmail.com>
2024-06-18 22:45:09 +0200

    Added translatable content driver

---
+++ src/FilamentAstrotomicTranslatableContentDriver.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace CorneelD\FilamentAstrotomicTranslatable;
+
+use Filament\Support\Contracts\TranslatableContentDriver;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class FilamentAstrotomicTranslatableContentDriver implements TranslatableContentDriver
+{
+    public function __construct(protected string $activeLocale)
+    {
+        //
+    }
+
+    public function isAttributeTranslatable(string $model, string $attribute): bool
+    {
+        $model = app($model);
+
+        if (! property_exists($model, 'isTranslationAttribute')) {
+            return false;
+        }
+
+        return $model->isTranslationAttribute($attribute);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getRecordAttributesToArray(Model $record): array
+    {
+        $recordData = $record->attributesToArray();
+
+        if (! property_exists($record, 'attributesToArray')) {
+            return $recordData;
+        }
+        $translatedData = $record->getTranslationsArray();
+
+        return array_merge($recordData, $translatedData);
+    }
+
+    /**
+     * @param  array<string, mixed>  $data
+     */
+    public function makeRecord(string $model, array $data): Model
+    {
+        $record = new $model();
+
+        $record->fill($data);
+
+        return $record;
+    }
+
+    public function setRecordLocale(Model $record): Model
+    {
+        if (!method_exists($record, 'setLocale')) {
+            return $record;
+        }
+
+        return $record->setLocale($this->activeLocale);
+    }
+
+    /**
+     * @param  array<string, mixed>  $data
+     */
+    public function updateRecord(Model $record, array $data): Model
+    {
+        $record->fill($data);
+
+        return $record;
+    }
+
+    public function applySearchConstraintToQuery(Builder $query, string $column, string $search, string $whereClause, ?bool $isCaseInsensitivityForced = null): Builder
+    {
+        if ($isCaseInsensitivityForced) {
+            $column = DB::raw("LOWER({$column})");
+            $search = strtolower($search);
+        }
+
+        return $query->{$whereClause . "TranslationLike"}($column, "%{$search}%", $this->activeLocale);
+    }
+}
