5037c5fd62f8055752c05ef1b379770e96e3ec16
Corneel D <corneel.d.git@gmail.com>
2024-07-03 20:55:55 +0200

    Added a RelationManager concern

---
+++ src/Resources/Pages/RelationManager/Concerns/Translatable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CorneelD\FilamentAstrotomicTranslatable\Resources\RelationManagers\Concerns;
+
+use CorneelD\FilamentAstrotomicTranslatable\Resources\Pages\Concerns\BaseTranslatable;
+
+trait Translatable
+{
+    use BaseTranslatable;
+}
